51da76a8313b620c23bf1a6626dd4e51bf936107
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-12-12 16:23:13 +0200

    adds static tasks

---
+++ src/ts/ToDo/tasks.ts
@@ -0,0 +1,32 @@
+export const tasks: { id: string; completed: boolean; body: string; title:string}[] = [
+  {
+    id: 'task-5203j93h4nt8u45g',
+    completed: true,
+    body:
+      'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
+    title: 'Eu ea incididunt sunt consectetur fugiat non.',
+  },
+  {
+    id: 'task-4354u8957438956',
+    completed: false,
+    body:
+      'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
+    title:
+      'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
+  },
+  {
+    id: 'task-d9fjg094780ywe809',
+    completed: true,
+    body:
+      'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
+    title: 'Eu ea incididunt sunt consectetur fugiat non.',
+  },
+  {
+    id: 'task-g895y495ht45hy495y984yu5',
+    completed: false,
+    body:
+      'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
+    title:
+      'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
+  },
+];
