e4b5f4dbd17b78cd6658c5188346725fc77a5f0c
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-12-13 15:23:41 +0200

    save styles

--- src/ts/ToDo/createTask.ts
+++ src/ts/ToDo/createTask.ts
@@ -6,6 +6,7 @@ export function listItemTemplate({ title, text, id }: TaskValues) {
     li.setAttribute('data-task-id', id);
 
     const taskTitle = document.createElement('h2');
+    taskTitle.classList.add('main__task-title');
     taskTitle.textContent = title;
   
 
@@ -14,6 +15,7 @@ export function listItemTemplate({ title, text, id }: TaskValues) {
     deleteBtn.textContent = 'Delete task';
 
     const taskText = document.createElement('p');
+    taskText.classList.add('main__task-text')
     taskText.textContent = text;
 
     li.appendChild(taskTitle);
