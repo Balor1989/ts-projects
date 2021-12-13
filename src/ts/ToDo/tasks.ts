c282c6f528ac9ab15ec69df0f9071ce42f43c6af
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-12-13 15:45:43 +0200

    save styles changes

--- src/ts/ToDo/tasks.ts
+++ src/ts/ToDo/tasks.ts
@@ -5,30 +5,30 @@ export const tasks: TaskValues[] = [
     id: 'task-5203j93h4nt8u45g',
     completed: true,
     text:
-      'Fringilla urna porttitor rhoncus dolor purus non enim. Ac tortor vitae purus faucibus ornare suspendisse sed. Mi quis hendrerit dolor magna eget est lorem ipsum. Vel pharetra vel turpis nunc eget lorem. Eu consequat ac felis donec et. Mollis aliquam ut porttitor leo a diam sollicitudin. Ac tincidunt vitae semper quis lectus. Montes nascetur ridiculus mus mauris vitae ultricies leo integer malesuada. Quisque sagittis purus sit amet volutpat. Sed viverra ipsum nunc aliquet bibendum enim. Turpis egestas sed tempus urna. Adipiscing at in tellus integer feugiat scelerisque. Eu nisl nunc mi ipsum faucibus. Dictum varius duis at consectetur lorem donec massa.',
-    title: 'Duis ut diam quam nulla porttitor.',
+      'Organize and maintain public information office files. Prepare mailings. Answer office phones/substitute for district receptionist.',
+    title: 'Clerical tasks',
   },
   {
     id: 'task-4354u8957438956',
     completed: false,
     text:
-      'Dictum varius duis at consectetur lorem donec. Tortor pretium viverra suspendisse potenti nullam. Nulla aliquet porttitor lacus luctus accumsan. Placerat in egestas erat imperdiet sed euismod nisi. Placerat vestibulum lectus mauris ultrices eros in cursus. Ante in nibh mauris cursus. Enim sed faucibus turpis in eu mi bibendum neque egestas. Facilisis magna etiam tempor orci. In vitae turpis massa sed. Id diam vel quam elementum pulvinar etiam non quam lacus. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin. Nibh ipsum consequat nisl vel. Arcu dictum varius duis at consectetur lorem donec. Nibh tortor id aliquet lectus. Enim eu turpis egestas pretium aenean pharetra magna. Id semper risus in hendrerit gravida rutrum quisque. Tortor consequat id porta nibh venenatis cras sed. Sit amet mattis vulputate enim nulla aliquet.',
+      'Collect information and design publications and flyers, including.Learning, Course Guide, My Favorite Teacher, Teacher Appreciation. Week flyer, school handbooks, school supply lists and other flyers. Create forms for schools and departments.',
     title:
-      'Ultricies leo integer malesuada nunc vel risus commodo viverra maecenas. ',
+      'Design tasks',
   },
   {
     id: 'task-d9fjg094780ywe809',
     completed: true,
     text:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat semper viverra nam libero justo laoreet. Massa enim nec dui nunc mattis. Ut venenatis tellus in metus. Egestas diam in arcu cursus euismod quis viverra nibh cras. Tellus pellentesque eu tincidunt tortor. Sit amet commodo nulla facilisi nullam vehicula ipsum. Magna ac placerat vestibulum lectus mauris. Turpis egestas pretium aenean pharetra magna. Ut sem viverra aliquet eget sit. Egestas erat imperdiet sed euismod. Quisque egestas diam in arcu cursus euismod. Pulvinar proin gravida hendrerit lectus. Molestie nunc non blandit massa. Quam elementum pulvinar etiam non quam lacus suspendisse. Sit amet mattis vulputate enim nulla. Vulputate enim nulla aliquet porttitor lacus luctus accumsan.',
-    title: 'Semper quis lectus nulla at.',
+      'Contact businesses to solicit support for Teacher Appreciation Week and Welcome Back give-a ways and activities and for Best Buddy program. Talk with parents who have questions or concerns.',
+    title: 'Public relations tasks',
   },
   {
     id: 'task-g895y495ht45hy495y984yu5',
     completed: false,
     text:
-      'Eu consequat ac felis donec et. Tincidunt arcu non sodales neque sodales. Nunc vel risus commodo viverra maecenas accumsan. Egestas diam in arcu cursus euismod quis viverra. Vel pharetra vel turpis nunc eget lorem. Iaculis nunc sed augue lacus viverra. Dignissim sodales ut eu sem integer vitae. Ultricies lacus sed turpis tincidunt. Sed enim ut sem viverra aliquet. Leo duis ut diam quam nulla.',
+      'Assist with creation of/maintain public information office budget. Enter purchase orders and make sure invoices are paid for public information office, including LearnTV.',
     title:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      'Bookkeeping tasks',
   },
 ];
