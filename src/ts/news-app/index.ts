1372ca303566e86fcb7556f7f6e27fce73345ef4
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-12-14 15:33:36 +0200

    adds two fetch (counry and word)

--- src/ts/news-app/index.ts
+++ src/ts/news-app/index.ts
@@ -1,13 +1,21 @@
 import '../../sass/news-app.scss'
 import axios from 'axios';
-const country: string = 'ua';
-const url: string = 'https://newsapi.org/v2/';
-
-const API_KEY: string = 'adccc341e74c4606857be1b9da45eafd';
 
 
-axios.get(`${url}top-headlines?country=${country}&apiKey=${API_KEY}`).then(response => {
-    const values = response.data;
-    const data = values
-     console.log(data)
-});
+function fetchNews(country: string = 'ua', search: string = 'health') {
+    const url: string = 'https://newsapi.org/v2/';
+    const API_KEY: string = 'adccc341e74c4606857be1b9da45eafd';
+        
+    axios.get(`${url}top-headlines?country=${country}&apiKey=${API_KEY}`).then(response => {
+            const values = response.data;
+            const data = values
+            console.log(data)
+    })
+    axios.get(`${url}top-headlines?q=${search}&apiKey=${API_KEY}`).then(response => {
+            const values = response.data;
+            const data = values
+            console.log(data)
+    })
+}
+fetchNews()
+
