59e49b4cf594b9cff8deb53be8bf7686316fec27
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-12-14 15:15:08 +0200

    save current fetch

--- src/ts/news-app/index.ts
+++ src/ts/news-app/index.ts
@@ -1,18 +1,13 @@
 import '../../sass/news-app.scss'
 import axios from 'axios';
-
-const url: string = 'https://newsapi.org/v2/everything?q=bitcoin&apiKey=';
+const country: string = 'ua';
+const url: string = 'https://newsapi.org/v2/';
 
 const API_KEY: string = 'adccc341e74c4606857be1b9da45eafd';
 
-interface ResponseValues{
-    status: string;
-    totalResults: number;
-    articles: {};
-}
 
-axios.get(`${url}${API_KEY}`).then(response => {
-    const values = response.data as ResponseValues;
+axios.get(`${url}top-headlines?country=${country}&apiKey=${API_KEY}`).then(response => {
+    const values = response.data;
     const data = values
      console.log(data)
 });
