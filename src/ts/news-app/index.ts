642b884c26705d8a68c6e3f78909f1fe8a9d071e
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-12-14 16:46:39 +0200

    adds hbr test template

--- src/ts/news-app/index.ts
+++ src/ts/news-app/index.ts
@@ -1,29 +1,24 @@
 import '../../sass/news-app.scss'
 import axios from 'axios';
+ 
+const url: string = 'https://newsapi.org/v2/';
+const API_KEY: string = 'adccc341e74c4606857be1b9da45eafd';
 
-
-function fetchNews(country: string = 'ua', search: string = 'health') {
-    const url: string = 'https://newsapi.org/v2/';
-    const API_KEY: string = 'adccc341e74c4606857be1b9da45eafd';
-        
+function fetchNewsbyCountry(country: string = 'ua'):void {
     axios.get(`${url}top-headlines?country=${country}&apiKey=${API_KEY}`).then(response => {
             const values = response.data;
             console.log(values.articles)
     })
+} 
+function fetchNewsbySearch(search: string):void {
     axios.get(`${url}top-headlines?q=${search}&apiKey=${API_KEY}`).then(response => {
-            const values = response.data;
-            console.log(values.articles)
+        const values = response.data;
+        console.log(values.articles)
     })
 }
-fetchNews()
-
+fetchNewsbyCountry('ua')
 function renderNewsBox(news) {
     const newsBox = document.querySelector('.news__card')
     news.forEach(element => {
-        const elementOfNews = newsTemplate(element)
     });
-}
-
-function newsTemplate(element) {
-console.log(element)
 }
