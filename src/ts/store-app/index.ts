1b6a8a102a3a5b2fcb94392938e3bc31e5cd0958
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-12-16 22:38:55 +0200

    save test response store app

---
+++ src/ts/store-app/index.ts
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+
+interface Country {
+  country_code: string;
+    name: string;
+    coordinates: {
+        lat: number,
+        lng: number
+    }
+}
+
+type CountryList = Country[]
+
+
+
+
+const siteLink: { url: string, apiToken: string } = {
+    url: 'https://aviasales-api.herokuapp.com/',
+    apiToken: 'e0798b45a1134979b576a54b7ede2b27'
+}
+
+class Api {
+    constructor(public config: {
+        url: string,
+        apiToken: string
+    }) { }
+    
+    async cities():Promise<Object> {
+        try {
+            const response = await axios.get<CountryList>(`${this.config.url}cities`)
+            console.log(response)
+            return response.data
+        }
+        catch (error) {
+            console.log(error)
+            return Promise.reject(error)
+       }
+     }
+    
+}
+const api = new Api(siteLink)
+
+api.cities().then(response => console.log(response))
