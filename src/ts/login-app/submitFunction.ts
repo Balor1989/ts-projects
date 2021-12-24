e5601d98763e45c3559c449795908f8bfec6c8a7
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-12-24 22:31:27 +0200

    adds notify to inputs

--- src/ts/login-app/submitFunction.ts
+++ src/ts/login-app/submitFunction.ts
@@ -1,17 +1,26 @@
 import { showInputError } from "./showInputErrors";
 import { validate } from "./validate";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const inputEmail: HTMLInputElement = document.querySelector('.form-section__input-email');
 const inputPassword: HTMLInputElement = document.querySelector('.form-section__input-password');
 
 export const inputs = [inputEmail, inputPassword];
 
-export function onSubmit(): boolean {
-    const isFormValid: boolean = inputs.some((input => {
-        let isValidInput = validate(input)
+export async function onSubmit() {
+    const isFormValid = inputs.every((input => {
+        const isValidInput = validate(input)
         if (!isValidInput) {
             showInputError(input)
+            Notify.warning('Incorrect input');
+            return false
+        }
+        if (isValidInput) {
+            return true
         }
     }))
+    if (isFormValid) {
+       Notify.success('Correct!');
+       }
     return isFormValid
 }
