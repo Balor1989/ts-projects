0eb0c03c4169c59833c10fb5b771e9f8c8d9d120
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-12-24 12:00:09 +0200

    adds form validation

--- src/ts/login-app/index.ts
+++ src/ts/login-app/index.ts
@@ -1,1 +1,23 @@
 import '../../sass/login-app.scss'
+import { validate } from './validate';
+
+const formEl: HTMLFormElement = document.querySelector('.form-section__login-form');
+const inputEmail: HTMLInputElement = document.querySelector('.form-section__input-email');
+const inputPassword: HTMLInputElement = document.querySelector('.form-section__input-password');
+
+console.log(formEl, inputPassword, inputEmail);
+
+formEl.addEventListener('submit', event => {
+    event.preventDefault()
+    onSubmit()
+    console.log(onSubmit())
+})
+
+function onSubmit():boolean {
+    const isFormValid = [inputPassword, inputEmail].every((input => {
+        const isValidInput = validate(input)
+        return isValidInput
+    }))
+    return isFormValid
+}
+
---
+++ src/ts/login-app/validate.ts
@@ -0,0 +1,10 @@
+const regExpDic:{email:RegExp, password:RegExp} = {
+  email: /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/,
+  password: /^[0-9a-zA-Z]{4,}$/,
+};
+
+export function validate(element: HTMLInputElement): boolean {
+  const RegExpName: string = element.dataset.required
+  if (!regExpDic[RegExpName]) return true
+  return regExpDic[RegExpName].test(element.value)
+}
